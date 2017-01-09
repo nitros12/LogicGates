96194c6c5f9e9c3a97496b1d88e2bb8a24046099
nitros12 <bsimms.simms@gmail.com>
2017-01-09 22:40:25 +0000

    idk fixed some stuff

--- LogicGates.py
+++ LogicGates.py
@@ -1,6 +1,7 @@
 import enum
 from itertools import product
 
+
 class bools(enum.Enum):
     true = 1
     false = 0
@@ -8,69 +9,79 @@ class bools(enum.Enum):
 
 class gate:
     rules = {}
+
     def __call__(self, *r):
         return self.rules.get(tuple(i for i in r))  #py3.4pls
 
+
 class or_gate(gate):
     rules = {
-        (bools.false, bools.false):bools.false,
-        (bools.false, bools.true):bools.true,
-        (bools.true, bools.false):bools.true,
-        (bools.true, bools.true):bools.true
-        }
+        (bools.false, bools.false): bools.false,
+        (bools.false, bools.true): bools.true,
+        (bools.true, bools.false): bools.true,
+        (bools.true, bools.true): bools.true
+    }
+
 
 class and_gate(gate):
     rules = {
-        (bools.false, bools.false):bools.false,
-        (bools.false, bools.true):bools.false,
-        (bools.true, bools.false):bools.false,
-        (bools.true, bools.true):bools.true
-        }
+        (bools.false, bools.false): bools.false,
+        (bools.false, bools.true): bools.false,
+        (bools.true, bools.false): bools.false,
+        (bools.true, bools.true): bools.true
+    }
+
 
 class xor_gate(gate):
     rules = {
-        (bools.false, bools.false):bools.false,
-        (bools.false, bools.true):bools.true,
-        (bools.true, bools.false):bools.true,
-        (bools.true, bools.true):bools.false
-        }
+        (bools.false, bools.false): bools.false,
+        (bools.false, bools.true): bools.true,
+        (bools.true, bools.false): bools.true,
+        (bools.true, bools.true): bools.false
+    }
+
 
 class not_gate(gate):
-    rules = {
-        (bools.false):bools.true,
-        (bools.true):bools.false
-        }
+    rules = {(bools.false): bools.true, (bools.true): bools.false}
+
 
 class nand_gate(gate):
     rules = {
-        (bools.false, bools.false):bools.true,
-        (bools.false, bools.true):bools.true,
-        (bools.true, bools.false):bools.true,
-        (bools.true, bools.true):bools.false
-        }
+        (bools.false, bools.false): bools.true,
+        (bools.false, bools.true): bools.true,
+        (bools.true, bools.false): bools.true,
+        (bools.true, bools.true): bools.false
+    }
+
 
 class nor_gate(gate):
     rules = {
-        (bools.false, bools.false):bools.true,
-        (bools.false, bools.true):bools.false,
-        (bools.true, bools.false):bools.false,
-        (bools.true, bools.true):bools.false
-        }
+        (bools.false, bools.false): bools.true,
+        (bools.false, bools.true): bools.false,
+        (bools.true, bools.false): bools.false,
+        (bools.true, bools.true): bools.false
+    }
+
 
 class inp_out_src:
+    def __init__(self):
+        self.outputs = []
+        self.inputs = []
+
     def add_output(self, output):
         self.outputs.append(output)
 
     def add_input(self, input_):
         self.inputs.append(input_)
-    
+
+
 class connection:
     def __init__(self, from_, to):
         self.from_ = from_
         from_.add_output(self)
         self.to = to
         to.add_input(self)
-        self.val = None
+        self.val = bools.false
 
     def receive(self, val):
         self.val = val
@@ -78,12 +89,12 @@ class connection:
 
     def __bool__(self):
         return self.val == bools.true
-        
+
+
 class active_gate(inp_out_src):
     def __init__(self, gate):
+        super().__init__()
         self.gate = gate()
-        self.inputs = []
-        self.outputs = []
 
     def update(self):
         for i in self.outputs:
@@ -92,8 +103,8 @@ class active_gate(inp_out_src):
 
 class static_value(inp_out_src):
     def __init__(self, val):
+        super().__init__()
         self.val = val
-        self.outputs = []
 
     def update(self):
         for i in self.outputs:
@@ -103,37 +114,40 @@ class static_value(inp_out_src):
         self.val = value
         self.update()
 
-class output(inp_out_src):
-    def __init__(self, input_):
-        self.input_ = input_
-        input_.add_output(self)
 
-    def receive(self, val):
-        self.val = val
+class output(inp_out_src):
+    def __init__(self):
+        super().__init__()
+        self.val = bools.false
 
     def update(self):
-        print(any(self.inputs))
+        self.val = bools.true if any((i.val.value
+                                      for i in self.inputs)) else bools.false
 
 
-xor = active_gate(xor_gate)
-and_ = active_gate(and_gate)
+if __name__ == "__main__":
+    xor = active_gate(xor_gate)
+    and_ = active_gate(and_gate)
 
+    x = static_value(bools.false)
+    y = static_value(bools.true)
 
-x = static_value(bools.false)
-y = static_value(bools.true)
+    out_s = output()
+    out_c = output()
 
-x_to_xor = connection(x, xor)
-y_to_xor = connection(y, xor)
+    x_to_xor = connection(x, xor)
+    y_to_xor = connection(y, xor)
 
-x_to_and = connection(x, and_)
-y_to_and = connection(y, and_)
+    x_to_and = connection(x, and_)
+    y_to_and = connection(y, and_)
 
-out_s = output(xor)
-out_c = output(and_)
+    xor_to_out_s = connection(xor, out_s)
+    and_to_out_c = connection(and_, out_c)
 
-combs = product([bools.false, bools.true], repeat=2)
+    combs = product([bools.false, bools.true], repeat=2)
 
-for x_, y_ in combs:
-    x.set(x_)
-    y.set(y_)
-    print("x:{}, y:{}, s:{}, c:{}".format(x.val.value, y.val.value, out_s.val.value, out_c.val.value))
+    for x_, y_ in combs:
+        x.set(x_)
+        y.set(y_)
+        print("x:{}, y:{}, s:{}, c:{}".format(
+            x.val.value, y.val.value, out_s.val.value, out_c.val.value))
